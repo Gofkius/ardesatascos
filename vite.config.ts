bec6278412a96c33874524f0d63c31ffbf02e12d
gofkius <eizosas@gmail.com>
2026-06-28 09:03:36 +0100

    Updated favicons and created support for Cloudflare workers.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   plugins: [
+    cloudflare(),
     tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] })
